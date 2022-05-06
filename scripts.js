09d39ac0ddd77e8987a31b71bb8ea02cca8dd6e8
p261225 <76599961+p261225@users.noreply.github.com>
2022-05-06 18:08:20 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-menuToggler.addEventListener('click', ev => {
-	menuToggler.classList.toggle('open');
-});
-
-for (const element of document.querySelectorAll('nav a')) {
-	element.addEventListener('click', ev => {
-		menuToggler.classList.remove('open');
-	});
-}
